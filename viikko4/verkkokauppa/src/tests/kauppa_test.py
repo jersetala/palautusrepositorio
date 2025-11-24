cbe1befce37982a95bdcf2ce23a868b0cbb8ebd8
Jeremias Setälä <jeremias.setala@helsinki.fi>
2025-11-24 13:09:02 +0200

    fix previous commit and add test for poista_korista

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -129,10 +129,17 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("none", "11111")
 
-        self.viitegeneraattori_mock.uusi.assert_called()
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 1)
 
         self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(2)
         self.kauppa.tilimaksu("none", "22222")
 
-        self.viitegeneraattori_mock.uusi.assert_called()
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
+
+    def test_peruutettu_asiointi_palauttaa_esineen_varastoon(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.varasto_mock.ota_varastosta.assert_called()
+        self.kauppa.poista_korista(1)
+        self.varasto_mock.palauta_varastoon.assert_called()
