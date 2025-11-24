73a47ac6e8c395354f3d42e3ca450d0e2db188eb
Jeremias Setälä <jeremias.setala@helsinki.fi>
2025-11-24 11:56:59 +0200

    add tests for Kassapaate.lataa

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_lisataan_saldoa(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called_with(10)
+
+    def tests_kortille_ei_voi_lisata_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
