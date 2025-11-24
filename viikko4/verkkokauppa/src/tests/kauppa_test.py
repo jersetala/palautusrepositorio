1d9523a649a9f240e32dd621c90a21824b1fc3f0
Jeremias Setälä <jeremias.setala@helsinki.fi>
2025-11-24 12:13:16 +0200

    add test for buying an existing product asserting that the correct bank transfer is done

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -4,6 +4,7 @@ from kauppa import Kauppa
 from viitegeneraattori import Viitegeneraattori
 from varasto import Varasto
 from tuote import Tuote
+import viitegeneraattori
 
 class TestKauppa(unittest.TestCase):
     def test_maksettaessa_ostos_pankin_metodia_tilisiirto_kutsutaan(self):
@@ -40,3 +41,31 @@ class TestKauppa(unittest.TestCase):
         # varmistetaan, että metodia tilisiirto on kutsuttu
         pankki_mock.tilisiirto.assert_called()
         # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
+
+    def test_maksettaessa_ostos_pankin_metodia_tilisiirto_kutsutaan_argumenteilla(self):
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        viitegeneraattori_mock.uusi.return_value = 42
+
+        varasto_mock = Mock()
+
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "Juusto", 10)
+
+
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("essi", "54321")
+
+        pankki_mock.tilisiirto.assert_called_with("essi", 42, "54321", ANY, 10)
